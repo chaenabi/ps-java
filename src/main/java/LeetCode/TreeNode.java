36dfd5d79bf24bb29711be9eed84edbe8171325b
Charlie <find1086@gmail.com>
2021-07-30 17:30:08 +0900

    clean code

--- src/main/java/LeetCode/TreeNode.java
+++ src/main/java/LeetCode/TreeNode.java
@@ -18,16 +18,3 @@ public class TreeNode {
         this.right = right;
     }
 }
-
-class Test {
-
-    public static void main(String[] args) {
-
-        TreeNode tr = new TreeNode(1);
-        tr.left = new TreeNode(2);
-        tr.right = new TreeNode(3);
-
-    }
-
-}
-
