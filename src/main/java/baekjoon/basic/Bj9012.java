980b51215b44892a4f752dc42176a6aa74882b51
chaenabi <find1086@gmail.com>
2021-11-15 22:08:14 +0900

    solve problems

---
+++ src/main/java/baekjoon/basic/Bj9012.java
@@ -0,0 +1,80 @@
+package baekjoon.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 괄호
+ * www.acmicpc.net/problem/9012
+ * 2021-11-15
+ */
+public class Bj9012 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<String> tokens = new LinkedList<>();
+
+        int count = Integer.parseInt(br.readLine()); // 6
+
+        while (count-- > 0) {
+            tokens.offer(br.readLine());
+        }
+        br.close();
+
+        while (!tokens.isEmpty()) {
+            Stack<Character> openPth = new Stack<>();
+
+            String token = tokens.poll();
+            char[] chars = new char[token.length()];
+            for (int i = 0; i < token.length(); i++) {
+                chars[i] = token.charAt(i); // ( ( ( (//4 )//3 (//4 )//3 )//2 (//1 )//0
+            }
+
+            boolean flag = false;
+
+            // 메인로직: ( 이면, 스택에 넣고, ) 스택에서 pop.
+            // 스택이 비어있는데 ) 이면 NO 출력
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] == '(') {
+                    openPth.push(chars[i]);
+                }
+                else {
+                    if (!openPth.empty()) openPth.pop();
+                    else {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if (flag) System.out.println("NO");
+            else if (openPth.empty()) System.out.println("YES");
+            else if (!openPth.empty()) System.out.println("NO");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/baekjoon/basic/Bj9093.java
@@ -0,0 +1,71 @@
+package baekjoon.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * 단어 뒤집기
+ * www.acmicpc.net/problem/9093
+ * 2021-11-15
+ */
+public class Bj9093 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<String> tokens = new LinkedList<>();
+
+        int count = Integer.parseInt(br.readLine());
+
+        while (count-- > 0) {
+            tokens.offer(br.readLine());
+        }
+        br.close();
+
+        while (!tokens.isEmpty()) {
+            String[] strings = tokens.poll().split(" "); // [I, am, happy, today]
+            List<String> list = new ArrayList<>();
+            int size = strings.length; // total strings.len (4)
+
+            for (int i = 0; i < size; i++) {
+                if (strings[i].length() == 1) {
+                    list.add(strings[i]);
+                } else {
+                    list.add(swap(strings[i]));
+                }
+            }
+
+            for (String s : list) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static String swap(String str) {
+        char[] chars = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            chars[i] = str.charAt(i);
+        }
+
+        int i = 0;
+        int j = chars.length - 1;
+        while (i < j) {
+            char c = chars[i];
+            chars[i] = chars[j];
+            chars[j] = c;
+            i++;
+            j--;
+        }
+
+        return String.valueOf(chars);
+    }
+}
+
+
+
+
+
