c4ebcdf4f75790f513ab40890810c0bc12e1409f
chaenabi <find1086@gmail.com>
2021-11-26 08:31:17 +0900

    solve bj2609: gcd and lcm

---
+++ src/main/java/baekjoon/basic/Bj2609.java
@@ -0,0 +1,37 @@
+package baekjoon.basic;
+
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * gcd, lcm
+ * https://www.acmicpc.net/problem/2609
+ */
+public class Bj2609 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        br.close();
+
+        int a = inputs[0];
+        int b = inputs[1];
+
+        int result = gcd(a, b);
+
+        bw.write(result + "\n");
+        bw.write(((a * b) / result) + "");
+
+        bw.flush();
+        bw.close();
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
