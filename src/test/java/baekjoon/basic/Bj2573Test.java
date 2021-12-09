a02df0c57d757403a5c4420b09bce31271b4dc8c
chaenabi <find1086@gmail.com>
2021-12-09 10:11:55 +0900

    충돌 정상화

--- src/test/java/baekjoon/basic/Bj2573Test.java
+++ src/test/java/baekjoon/basic/Bj2573Test.java
@@ -1,6 +1,5 @@
 package baekjoon.basic;
 
-<<<<<<< HEAD
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -43,27 +42,4 @@ class Bj2573Test {
         assertThat(bj2573.solution(7, 7, map)).isEqualTo(2);
     }
 
-
-
-=======
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-class Bj2573Test {
-
-    Bj2573 bj2573 = new Bj2573();
-
-    @Test
-    public void case1() {
-        assertThat(bj2573.solution(5, 7,
-                new int[][]{
-                        {0, 0, 0, 0, 0, 0, 0},
-                        {0, 3, 3, 2, 3, 3, 0},
-                        {0, 4, 0, 4, 0, 3, 0},
-                        {0, 0, 0, 0, 4, 3, 0},
-                        {0, 0, 0, 0, 0, 0, 0}}
-                )).isEqualTo(0);
-    }
->>>>>>> 634f92b032251488ab15cf1cb3d1ee4de91bc5d1
 }
