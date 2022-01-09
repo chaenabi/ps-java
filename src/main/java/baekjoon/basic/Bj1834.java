ad94c9edc428f4675fcc03d3db8269da42018751
chaenabi <find1086@gmail.com>
2022-01-09 18:20:27 +0900

    solve bj1834

--- src/main/java/baekjoon/basic/Bj1834.java
+++ src/main/java/baekjoon/basic/Bj1834.java
@@ -1,2 +1,11 @@
-package baekjoon.basic;public class Bj1834 {
+package baekjoon.basic;
+
+import java.io.*;
+
+public class Bj1834 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n * ~-n * -~n / 2);
+    }
 }
