b5428967bae36cbeea8729fe0e38a4106dd2814d
chaenabi <find1086@gmail.com>
2022-01-13 00:57:58 +0900

    solve leet701

---
+++ src/main/java/LeetCode/Leet701.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+public class Leet701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (root.val > val) root.left = insertIntoBST(root.left, val);
+        if (root.val <= val) root.right = insertIntoBST(root.right, val);
+        return root;
+    }
+}
